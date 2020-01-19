b76b7eb785a2fa13fd2a21e4c5d78ba35cf2e279
AvenThia <thiaaven@gmail.com>
2020-01-19 11:42:13 +0800

    Update submit form fields

--- form/app.js
+++ form/app.js
@@ -22,14 +22,21 @@ function formSubmit(e) {
   e.preventDefault();
   // Get Values from the DOM
   let name = document.querySelector('#name').value;
+  let contact = document.querySelector('#contact').value;
   let email = document.querySelector('#email').value;
-  let password = document.querySelector('#password').value;
+  let linkedin = document.querySelector('#linkedin').value;
   let bio = document.querySelector('#bio').value;
-  let job = document.querySelector('#job').value;
-  let interest = document.querySelector('#interest').value;
+  let school = document.querySelector('#school').value;
+  let period = document.querySelector('#period').value;
+  let pg_name = document.querySelector('#pg_name').value;
+  let company_name = document.querySelector('#company_name').value;
+  let exp_period = document.querySelector('#exp_period').value;
+  let exp_Description = document.querySelector('#exp_Description').value;
+  let cca_Description = document.querySelector('#cca_Description').value;
+  let skills_Description = document.querySelector('#skills_Description').value;
 
   //send message values
-  sendMessage(name, email, password, bio, job, interest);
+  sendMessage(name, contact, email, linkedin, bio, school, period, pg_name, company_name, exp_period, exp_Description, cca_Description, skills_Description);
 
   //Show Alert Message(5)
   document.querySelector('.alert').style.display = 'block';
@@ -50,16 +57,23 @@ function redirect() {
 
 //Send Message to Firebase(4)
 
-function sendMessage(name, email, password, bio, job, interest) {
+function sendMessage(name, contact, email, linkedin, bio, school, period, pg_name, company_name, exp_period, exp_Description, cca_Description, skills_Description) {
   
   let newFormMessage = formMessage.push();
   newFormMessage.set({
     name: name,
+    contact: contact,
     email: email,
-    password: password,
+    linkedin: linkedin,
     bio: bio,
-    job: job,
-    Interest: interest
+    school: school,
+    period: period,
+    pg_name: pg_name,
+    company_name: company_name,
+    exp_period: exp_period,
+    exp_Description: exp_Description,
+    cca_Description: cca_Description,
+    skills_Description: skills_Description
   });
   redirect();
   console.log(name);
